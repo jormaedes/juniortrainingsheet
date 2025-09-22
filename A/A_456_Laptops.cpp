05899ab528dbd2eba0ebc289fc77036fcd683969
Jormaedes Luís <118800141+jormaedes@users.noreply.github.com>
2025-09-22 20:23:53 +0100

    Create A_456_Laptops.cpp

---
+++ A/A_456_Laptops.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n;
+    cin >> n;
+    vector<pair<int,int>> v(n);
+    
+    for(int i = 0; i < n; i++) cin >> v[i].first >> v[i].second;
+    sort(v.begin(), v.end());
+    for(int i = 1; i < n; i++) 
+    {
+        if(v[i].second < v[i-1].second)
+        {
+            cout << "Happy Alex";
+            return 0;
+        }
+    }
+    cout << "Poor Alex";
+}
